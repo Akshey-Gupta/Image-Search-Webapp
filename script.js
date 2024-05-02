0dc1571b14eae116210a43010fd3e739235e061d
Akshey-Gupta <100192799+Akshey-Gupta@users.noreply.github.com>
2024-05-02 20:41:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const apikey='1BuIAGV1pH8_tdgOg8u6DpyHuE7opavjPnn5UegKSbM'
+
+const formElement=document.querySelector('form')
+const inputElement=document.querySelector('.search-input')
+const searchResults = document.querySelector('.searchresults');
+const showMore=document.getElementById('button')
+
+let inputData=''
+let page=1;
+
+async function searchImages(){
+    inputData=inputElement.value;
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${apikey}`
+    
+    const response=await fetch(url)
+    const data= await response.json()
+
+    const results=data.results
+
+    if(page===1){
+        searchResults.innerHTML=''
+    }
+    results.map((result)=>{
+        const imageWrapper=document.createElement('div')
+        imageWrapper.classList.add('search-result')
+        const image=document.createElement('img')
+        image.src=result.urls.small
+        image.alt=result.alt_description
+        const imageLink=document.createElement('a')
+        imageLink.href=result.links.html
+        imageLink.target="_blank"
+        imageLink.textContent=result.alt_description
+
+        imageLink.appendChild(image)
+        imageWrapper.appendChild(imageLink)
+        searchResults.appendChild(imageWrapper)
+    })
+    page++
+    if(page>1)
+    showMore.style.display='block'
+}
+formElement.addEventListener('submit',function(event){
+    event.preventDefault()
+    page=1
+    searchImages()
+})
+showMore.addEventListener('click',function(){
+    searchImages()
+})
